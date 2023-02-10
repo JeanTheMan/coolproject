fec02afda2c4b85f3605c48056ea031e2e023ee9
JeanTheMan <125032297+JeanTheMan@users.noreply.github.com>
2023-02-10 11:54:48 +0000

    Start With This
    New

---
+++ main.py
@@ -0,0 +1,12 @@
+import textrender as tr
+from time import sleep
+#component()
+elines = tr.editablelist([])
+tr.setstartscreen(elines)
+pic = tr.strtopic('''O       O
+{_______}''')
+tr.picture(7, 7, pic.picture, elines)
+tr.component(3, 4 ,6 ,6, elines)
+pic = tr.strtopic('Yay')
+tr.picture(3, 13, pic.picture, elines)
+tr.updatescreen(elines.list)
