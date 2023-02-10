c544ea1225024e359d02d91b32b1916ee39b3336
JeanTheMan <125032297+JeanTheMan@users.noreply.github.com>
2023-02-10 12:16:09 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,13 @@
-from os import system, name
-
-def updatescreen(lines):
-  system('cls' if name == 'nt' else 'clear')
-  d = ''
-  for i in range(66):
-      d = d + '█'
-  print(d)
-  for i in lines:
-    print(f'█{i}█')
-  print(d)
-
-class editablelist:
-  def __init__(self, list):
-    self.list = list
-
-
-
-def setstartscreen(list):
-  d =  ''
-  for i in range(24):
-    d = ''
-    for i in range(64):
-      d = d + '⠀'
-      #d = d + '/'
-    list.list.append(d)
-
-class component:
-  def __init__(self, x, y, width, height, screen):
-    for xi in range(height):
-      line = screen.list[y + xi]
-      objstr = ''
-      for i in range(width):
-        objstr = f'{objstr}█'
-      screen.list[y + xi] = f'{line[:x]}{objstr}{line[(x + width):]}'
-
-class picture:
-  def __init__(self, x, y, picture, screen):
-    for xi in range(len(picture)):
-      line = screen.list[y + xi]
-      objstr = picture[xi]
-      width = len(objstr)
-      screen.list[y + xi] = f'{line[:x]}{objstr}{line[(x + width):]}'
-
-class strtopic:
-  def __init__(self, picture):
-    self.picture = picture.split('\n')
-      
+import textrender as tr
+from time import sleep
+#component()
+elines = tr.editablelist([])
+tr.setstartscreen(elines)
+pic = tr.strtopic('''O       O
+{_______}''')
+tr.picture(7, 7, pic.picture, elines)
+tr.component(3, 4 ,6 ,6, elines)
+pic = tr.strtopic('Yay')
+tr.picture(3, 13, pic.picture, elines)
+tr.updatescreen(elines.list)
     
