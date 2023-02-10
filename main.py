c0d7dcf110945e0fff3ad3f7dd50a605dd792de1
JeanTheMan <125032297+JeanTheMan@users.noreply.github.com>
2023-02-10 12:18:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 import textrender as tr
 from time import sleep
+import requests
 #component()
 elines = tr.editablelist([])
 tr.setstartscreen(elines)
-pic = tr.strtopic('''O       O
-{_______}''')
+pic = tr.strtopic(""".-.   .-..-.   .-..----.            
+| |   | ||  `.'  || {_              
+| `--.| || |\ /| || {__             
+`----'`-'`-' ` `-'`----'            
+ .----. .-. .-.  .--.  .----. .----.
+/  {}  \|  `| | / {} \ | {}  \| {_  
+\      /| |\  |/  /\  \|     /| {__ 
+ `----' `-' `-'`-'  `-'`----' `----'""")
 tr.picture(7, 7, pic.picture, elines)
-tr.component(3, 4 ,6 ,6, elines)
-pic = tr.strtopic('Yay')
-tr.picture(3, 13, pic.picture, elines)
+#tr.component(3, 4 ,6 ,6, elines)
+pic = tr.strtopic('Type "start" to start')
+tr.picture(15, 15, pic.picture, elines)
 tr.updatescreen(elines.list)
-    
+started = False
+while not started:
+  i = input().lower()
+  if i == 'start':
+    started = True
+  else:
+    tr.updatescreen(elines.list)
